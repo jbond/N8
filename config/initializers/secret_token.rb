36a63149237d77ebb48cc8be012dee0bba7185b6
James Bond <jamesbond.home@gmail.com>
2011-11-15 09:52:16 -0800

    initial commit - generated code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+N8::Application.config.secret_token = 'f63b6f262d0ce7d38d6e2af8aa7bb539d5bb63462cbd2742aa7f4f7f7343fd87979e7b2ce25daaeac8185baf17249979dc68155179744765dd9e1989094590ba'
